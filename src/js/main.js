e4b9a3d62c665bc8f8d76c9950ea43b65b0184d9
langerdaniel9 <langerdaniel19@gmail.com>
2021-05-17 17:10:13 -0400

    fixed ratings

--- src/js/main.js
+++ src/js/main.js
@@ -70,7 +70,7 @@ async function getTMDBInfo() {
         datasets: [
           {
             data: [rating2, 10 - rating2],
-            backgroundColor: ["rgba(64, 253, 19, 1)"],
+            backgroundColor: ["rgba(23, 191, 68, 1)"],
             borderColor: ["rgba(0,0,0,0)"],
             borderWidth: 0,
           },
@@ -104,6 +104,91 @@ async function getTMDBInfo() {
         hover: {
           mode: null,
         },
+      },
+    });
+
+    Chart.pluginService.register({
+      beforeDraw: function (chart) {
+        if (chart.config.options.elements.center) {
+          // Get ctx from string
+          var ctx = chart.chart.ctx;
+
+          // Get options from the center object in options
+          var centerConfig = chart.config.options.elements.center;
+          var fontStyle = centerConfig.fontStyle || "Montserrat-Bold";
+          var txt = centerConfig.text;
+          var color = centerConfig.color || "rgba(28, 28, 28, 1)";
+          var maxFontSize = centerConfig.maxFontSize || 50;
+          var sidePadding = centerConfig.sidePadding || 20;
+          var sidePaddingCalculated =
+            (sidePadding / 100) * (chart.innerRadius * 2);
+          // Start with a base font of 20px
+          ctx.font = "20px " + fontStyle;
+
+          // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+          var stringWidth = ctx.measureText(txt).width;
+          var elementWidth = chart.innerRadius * 2 - sidePaddingCalculated;
+
+          // Find out how much the font can grow in width.
+          var widthRatio = elementWidth / stringWidth;
+          var newFontSize = Math.floor(20 * widthRatio);
+          var elementHeight = chart.innerRadius * 2;
+
+          // Pick a new font size so it will not be larger than the height of label.
+          var fontSizeToUse = Math.min(newFontSize, elementHeight, maxFontSize);
+          var minFontSize = centerConfig.minFontSize;
+          var lineHeight = centerConfig.lineHeight || 25;
+          var wrapText = false;
+
+          if (minFontSize === undefined) {
+            minFontSize = 15;
+          }
+
+          if (minFontSize && fontSizeToUse < minFontSize) {
+            fontSizeToUse = minFontSize;
+            wrapText = true;
+          }
+
+          // Set font settings to draw it correctly.
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          var centerX = (chart.chartArea.left + chart.chartArea.right) / 2;
+          var centerY = (chart.chartArea.top + chart.chartArea.bottom) / 2;
+          ctx.font = fontSizeToUse + "px " + fontStyle;
+          ctx.fillStyle = color;
+
+          if (!wrapText) {
+            ctx.fillText(txt, centerX, centerY);
+            return;
+          }
+
+          var words = txt.split(" ");
+          var line = "";
+          var lines = [];
+
+          // Break words up into multiple lines if necessary
+          for (var n = 0; n < words.length; n++) {
+            var testLine = line + words[n] + " ";
+            var metrics = ctx.measureText(testLine);
+            var testWidth = metrics.width;
+            if (testWidth > elementWidth && n > 0) {
+              lines.push(line);
+              line = words[n] + " ";
+            } else {
+              line = testLine;
+            }
+          }
+
+          // Move the center up depending on line height and number of lines
+          centerY -= (lines.length / 2) * lineHeight;
+
+          for (var n = 0; n < lines.length; n++) {
+            ctx.fillText(lines[n], centerX, centerY);
+            centerY += lineHeight;
+          }
+          //Draw text in center
+          ctx.fillText(line, centerX, centerY);
+        }
       },
     });
 
@@ -149,7 +234,7 @@ async function getTVMazeInfo() {
         datasets: [
           {
             data: [rating1, 10 - rating1],
-            backgroundColor: ["rgba(49, 253, 15, 1)"],
+            backgroundColor: ["rgba(23, 191, 68, 1)"],
             borderColor: ["rgba(0,0,0,0)"],
             borderWidth: 0,
           },
@@ -196,7 +281,7 @@ async function getTVMazeInfo() {
         datasets: [
           {
             data: [9.2, 10 - 9.2],
-            backgroundColor: ["rgba(40, 254, 12, 1)"],
+            backgroundColor: ["rgba(23, 191, 68, 1)"],
             borderColor: ["rgba(0,0,0,0)"],
             borderWidth: 0,
           },
@@ -217,7 +302,7 @@ async function getTVMazeInfo() {
         },
         elements: {
           center: {
-            text: `${rating1}/10`,
+            text: `9.2/10`,
             fontColor: "rgba(0, 0, 0, 1)",
             sidePadding: 20, // Default is 20 (as a percentage)
             minFontSize: 15, // Default is 20 (in px), set to false and text will not wrap.
