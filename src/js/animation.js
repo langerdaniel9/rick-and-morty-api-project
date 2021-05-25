309692fa3cf3e8bd314ef9c5e24e4d0835f46b7e
coolwick <amber.li21@sitechhs.com>
2021-05-25 15:31:37 -0400

    somethings wrong

--- src/js/animation.js
+++ src/js/animation.js
@@ -9,40 +9,31 @@ const navOpen = document.querySelector(".nav-open");
 
 const tl = new TimelineLite({ paused: true, reversed: true });
 
-tl.to(".cover", 3, {
-  width: "65%",
-  opacity: "0",
-  ease: Power2.easeOut,
-})
-  .to(
-    "nav",
-    1,
-    {
-      height: "100%",
-      ease: Power2.easeOut,
+tl.to(
+  "nav",
+  1,
+  {
+    width: "100%",
+    ease: Power2.easeOut,
+  },
+  "-=.5"
+).fromTo(
+  ".nav-open",
+  0.5,
+  {
+    opacity: 0,
+    x: -50,
+    ease: Power2.easeOut,
+  },
+  {
+    opacity: 1,
+    x: 0,
+    onComplete: function () {
+      navOpen.style.pointerEvents = "auto";
+      console.log("done");
     },
-    "-=0.5"
-  )
-  .fromTo(
-    ".nav-open",
-    0.5,
-    {
-      opacity: 0,
-      x: 50,
-      ease: Power2.easeOut,
-    },
-    {
-      opacity: 1,
-      x: 0,
-      onComplete: function () {
-        navOpen.style.pointerEvents = "auto";
-        console.log("done");
-      },
-    }
-  );
-function toggleTween(tween) {
-  tween.reverse() ? tween.play : tween.reverse();
-}
+  }
+);
 navButton.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("clicked");
@@ -53,3 +44,7 @@ navButton.addEventListener("click", (e) => {
   }
   toggleTween(tl);
 });
+
+function toggleTween(tween) {
+  tween.reverse() ? tween.play : tween.reversed();
+}
